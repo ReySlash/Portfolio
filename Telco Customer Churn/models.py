bd7838d88b6cbfc2cabeebd17a96b29ef603aa14
ReySlash <rcarmenate@gmail.com>
2024-11-30 04:21:33 +0300

    Telco_customer_churn modeling finished

---
+++ Telco Customer Churn/models.py
@@ -0,0 +1,174 @@
+import sys
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_score, cross_validate
+
+from utils import Utils
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, roc_auc_score, make_scorer, f1_score, balanced_accuracy_score
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+
+
+
+class Models:
+
+    def __init__(self):
+        # Dictionary containing model names and their respective instances
+        self.clas = {
+            'logistic_regression': LogisticRegression(random_state=42),  # Logistic Regression model
+            'random_forest': RandomForestClassifier(random_state=42),    # Random Forest model
+            'svm': SVC(probability=True, random_state=42),               # Support Vector Machine model with probabilities enabled
+        }
+
+        # Dictionary containing hyperparameter grids for each model
+        self.params = {
+            'logistic_regression': {
+                'pca__n_components': [None],                      # Number of PCA components (optional)            
+                # 'model__penalty': ['l2', 'l1', None],                           # Type of regularization (l2 for Ridge regularization, l1 for Lasso)
+                # 'model__C': [0.1, 1, 10],                                       # Inverse of regularization strength; smaller values indicate more regularization
+                'model__solver': ['liblinear', 'lbfgs', 'newton-cg', 'saga'],   # Optimization algorithm
+                # 'model__max_iter': [100, 200, 300],                             # Maximum number of iterations for optimization methods
+                'model__class_weight': [None, 'balanced'],                      # Adjust weights for imbalanced classes
+                # 'model__fit_intercept': [True, False],                          # Whether to include the intercept term
+                # 'model__intercept_scaling': [1, 10],                            # Scaling of the intercept term for certain solvers
+                # 'model__warm_start': [True, False]                              # Reuse the solution of the previous call to fit and add more estimators
+                'model__max_iter' : [100000]
+            },
+            'random_forest': {
+                'pca__n_components': [None],
+                'model__criterion': ['gini', 'entropy'],        # Split criterion
+                'model__n_estimators': list(range(20,200,20)),  # Number of trees in the forest
+                'model__max_depth': list(range(2,8,2)),  # Maximum depth of the trees
+                'model__min_samples_split': [2, 5, 10],  # Minimum number of samples required to split an internal node
+                'model__min_samples_leaf': [5, 6, 7],  # Minimum number of samples required to be at a leaf node
+                'model__max_features': ['sqrt', 'log2', None],  # Number of features to consider when looking for the best split
+                'model__bootstrap': [True],  # Whether bootstrap samples are used when building trees
+                # 'model__oob_score': [True, False],  # Whether to use out-of-bag samples to estimate the error
+                'model__class_weight': [None, 'balanced'],  # Handle imbalanced classes
+                'model__n_jobs': [-1],  # Number of processors to use (use -1 for all)
+            },
+            # Define the grid of hyperparameters for an SVM model
+            'svm': {
+                'pca__n_components': [None],  # No PCA applied in this configuration; keep all original features
+                'model__C': [0.1,1,10],  # Regularization parameter; higher values reduce regularization, potentially leading to overfitting
+                'model__kernel': ['linear'],  # Use 'linear' for simple relationships and 'rbf' for non-linear relationships
+                'model__gamma': ['scale', 'auto'],  # Kernel coefficient; 'scale' and 'auto' adapt based on data size and variance
+                'model__class_weight': [None, 'balanced'],  # Adjusts weights inversely proportional to class frequencies for imbalance
+            }
+        }
+
+    def grid_training(self, X, y, balanced = False):
+        """
+        Perform hyperparameter tuning and model evaluation using GridSearchCV.
+        Separates training and test data, handles evaluation, and identifies the best model.
+        """
+        best_score = float('-inf')  # Initialize the best score for comparison
+        best_model = None  # To store the best model
+        best_params = None  # To store the best hyperparameters
+
+        # Split the data into training and testing sets (70% train, 30% test)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+        
+        if balanced:
+            # Applying undersampling
+            undersampler = RandomUnderSampler(sampling_strategy='auto', random_state=42)
+            X_train, y_train = undersampler.fit_resample(X_train, y_train)
+                 
+        # Loop through each model
+        for name, clas in self.clas.items():
+            print('\n',f'#'*32, f'Evaluating: {name}','#'*32,'\n')
+            
+            # Create a pipeline combining PCA and the model
+            pipeline = Pipeline([
+                ('pca', PCA()),          # PCA for dimensionality reduction
+                ('model', clas)          # Model (e.g., logistic regression, random forest)
+            ])
+
+            # StratifiedKFold Cross Validation setup (5 folds)
+            skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+            # Ensure the model has associated parameters in the dictionary
+            if name not in self.params:
+                print(f"Warning: No hyperparameters found for {name}. Skipping...")
+                continue
+            
+            # Setting f1-score as main score metric
+                    
+            # Initialize GridSearchCV for hyperparameter tuning
+            grid_search = GridSearchCV(
+                pipeline,
+                self.params[name],
+                cv=skfold,                       # Use K-Fold cross-validation
+                n_jobs=-1,                      # Utilize all available processors
+                return_train_score=True,        # Return training scores
+                error_score='raise',            # Raise errors if encountered
+                scoring='balanced_accuracy'    
+            )
+
+            try:
+                # Fit the model using GridSearchCV
+                grid_search.fit(X_train, y_train)
+            except Exception as e:
+                print(f"Error while training {name}: {e}")
+                continue
+
+            # Display the best parameters and cross-validation score
+            print("Best parameters:", grid_search.best_params_)
+            print("Best cross-validation score:", grid_search.best_score_)
+
+            # Evaluate the best model on the training and test set
+            train_y_pred = grid_search.best_estimator_.predict(X_train)
+            train_accuracy = accuracy_score(y_train, train_y_pred)
+            train_balanced_accuracy = balanced_accuracy_score(y_train, train_y_pred)
+            print("Train Accuracy:", train_accuracy)
+            print("Train Balanced-Accuracy:", train_balanced_accuracy)
+            
+            test_y_pred = grid_search.best_estimator_.predict(X_test)
+            test_accuracy = accuracy_score(y_test, test_y_pred)
+            test_balanced_accuracy = balanced_accuracy_score(y_test, test_y_pred)
+            print("Test Accuracy:", test_accuracy)
+            print("Test Balanced-Accuracy:", test_balanced_accuracy)
+            
+            c_matrix = confusion_matrix(y_test, test_y_pred)
+            print(f"Confusion Matrix:\n{pd.DataFrame(c_matrix)}")
+            
+            # Classification Report
+            print("\nClassification Report:\n", classification_report(y_test, test_y_pred))
+
+            # If the model supports predict_proba, calculate AUC
+            if hasattr(grid_search.best_estimator_.named_steps['model'], 'predict_proba'):
+                y_pred_proba = grid_search.best_estimator_.predict_proba(X_test)[:, 1]
+                auc = roc_auc_score(y_test, y_pred_proba)
+                print(f"AUC: {auc}")
+            
+            
+            # Saving the best params for each class
+            csv_name = f"Best_{name}_model"
+            pd.DataFrame([grid_search.best_params_]).to_csv(f'models/{csv_name}.csv')
+            
+            # Update the best model if the current one performs better
+            if grid_search.best_score_ > best_score:
+                best_score = grid_search.best_score_
+                best_model = grid_search.best_estimator_
+                best_params = grid_search.best_params_
+
+        # Final output of the best model's parameters and score
+        print("-"*32, "\nBest model identified:","-"*32)
+        print(f"Parameters: {best_params}")
+        print(f"Score: {best_score}")
+        print('-'*64)
+        
+        # Export the best model
+        utils = Utils()
+        utils.model_export(best_model, best_score)
+        print("\nModel successfully exported.")
+
---
+++ Telco Customer Churn/telco_customer_churn/utils/params_optimizer.py
@@ -0,0 +1,311 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.neighbors import NearestNeighbors
+from itertools import product
+
+# Attempt to delete the custom accessor if it already exists
+try:
+    del pd.DataFrame.params_optimizer
+except AttributeError:
+    pass
+
+@pd.api.extensions.register_dataframe_accessor("params_optimizer")
+class params_optimizer(pd.DataFrame):
+
+
+    def plot_explained_variance(self):
+        pca = PCA(random_state=42)
+        X_reduced = pca.fit_transform(self)
+        cumulative_exp_var = pca.explained_variance_ratio_.cumsum()*100
+        sns.lineplot(
+            x=list(range(1,self.shape[1]+1)),
+            y=cumulative_exp_var,
+            marker='o',
+            color="r"
+        )
+        plt.yticks(list(range(5,101,5)))
+        plt.xticks(list(range(1,self.shape[1]+1)))
+        plt.xlabel("Number of Components")
+        plt.ylabel("Cumulative Explained Variance")
+        plt.grid()
+        
+    def pca_kmeans_elbow_study(self,pca_n_components_range, n_clusters_range):
+        # Define the range of PCA components and clusters
+        pca_n_components_range = list(range(10, 16))
+
+        # Loop over the number of PCA components
+        for n in pca_n_components_range:
+            pca = PCA(n_components=n, random_state=42)
+            X_pca = pca.fit_transform(self)
+            
+            # Store inertia and silhouette scores for each number of clusters
+            inertia_values = []
+            silhouette_scores = []  # Initialize silhouette scores within this loop
+
+            # Loop over the number of clusters
+            for k in n_clusters_range:
+                kmeans = KMeans(n_clusters=k, random_state=42)
+                kmeans.fit(X_pca)
+                
+                # Append inertia and silhouette score
+                inertia_values.append(kmeans.inertia_)
+                score = silhouette_score(X_pca, kmeans.labels_)
+                silhouette_scores.append(score)
+
+            # Plot the inertia (Elbow method) and silhouette score for each k
+            plt.figure(figsize=(12, 5))
+            
+            # Elbow Method Plot
+            plt.subplot(1, 2, 1)
+            plt.plot(n_clusters_range, inertia_values, marker='o')
+            plt.xlabel("Number of Clusters (k)")
+            plt.ylabel("Inertia")
+            plt.title(f"Elbow Method for Optimal k (PCA components = {n})")
+            plt.xticks(n_clusters_range)
+            plt.grid()
+            
+            # Silhouette Score Plot
+            plt.subplot(1, 2, 2)
+            plt.plot(n_clusters_range, silhouette_scores, marker='o', color='orange')
+            plt.xlabel("Number of Clusters (k)")
+            plt.ylabel("Silhouette Score")
+            plt.title(f"Silhouette Score vs. k (PCA components = {n})")
+            plt.xticks(n_clusters_range)
+            plt.grid()
+            
+            plt.tight_layout()
+            plt.show()
+
+
+    def pca_kmeans_study(self, pca_n_components_range = None, n_clusters_range=0):
+        results = []
+        if pca_n_components_range is None:
+            # Nested Loop over number of clusters
+            for n_clusters in n_clusters_range:
+                kmeans = KMeans(n_clusters=n_clusters, random_state=42)  # Initialize KMeans model
+                kmeans.fit(self)                                     # Only fit the KMeans model
+                
+                inertia = kmeans.inertia_                                # Get inertia for the current clustering
+                silhouette_avg = silhouette_score(self, kmeans.labels_)  # Compute silhouette score
+                
+                # Append results into a dictionary for each combination of n_components and n_clusters
+                results.append({
+                    'n_clusters': n_clusters, 
+                    'inertia': inertia,
+                    'silhouette_score': silhouette_avg,
+                })
+
+            return pd.DataFrame(results)
+        
+        else:
+            # Main Loop over number of components
+            for n_components in pca_n_components_range:
+                pca = PCA(n_components=n_components, random_state=42)  # Initialize PCA with the current number of components
+                data_pca = pca.fit_transform(self)                       # Apply PCA transformation on data
+                explained_variance_ratio = pca.explained_variance_ratio_[-1]    # Explained variance for the last component
+                cumulative_explained_variance_ratio = pca.explained_variance_ratio_.cumsum()[-1]  # Cumulative variance
+                
+                # Nested Loop over number of clusters
+                for n_clusters in n_clusters_range:
+                    kmeans = KMeans(n_clusters=n_clusters, random_state=42)  # Initialize KMeans model
+                    kmeans.fit(data_pca)                                     # Only fit the KMeans model
+                    
+                    inertia = kmeans.inertia_                                # Get inertia for the current clustering
+                    silhouette_avg = silhouette_score(data_pca, kmeans.labels_)  # Compute silhouette score
+                    
+                    # Append results into a dictionary for each combination of n_components and n_clusters
+                    results.append({
+                        'n_components': n_components,
+                        'n_clusters': n_clusters, 
+                        'explained_variance': explained_variance_ratio,
+                        'cumulative_explained_variance_ratio': cumulative_explained_variance_ratio,
+                        'silhouette_score': silhouette_avg,
+                        'inertia': inertia
+                    })
+        
+                    
+            results_df = pd.DataFrame(results)
+            
+            # Copy of the original results DataFrame for scaling purposes
+            tests_df = results_df.copy()
+
+            # Scaling the metrics for comparison
+            scaler = MinMaxScaler()
+            cols = ['scaled_n_components', 'scaled_n_clusters', 'scaled_explained_variance', 
+                    'scaled_cumulative_explained_variance_ratio', 'scaled_silhouette_score', 'scaled_inertia']
+            
+            # Scale metrics and create a new DataFrame with the scaled results
+            scaled_results = pd.DataFrame(scaler.fit_transform(tests_df), columns=cols)
+
+            # Create a score for explained variance
+            scaled_results['exp_var_Score'] = (scaled_results['scaled_explained_variance'] + 
+                                            scaled_results['scaled_cumulative_explained_variance_ratio']) / 2
+
+            # Create a score for silhouette coefficient and number of clusters
+            scaled_results['silhouette_n_clust_score'] = (scaled_results['scaled_silhouette_score'] + 
+                                                        (1 - scaled_results['scaled_n_clusters'])) / 2
+
+            # Combine both scores to create a total score
+            scaled_results['total_score'] = (scaled_results['exp_var_Score'] + 
+                                            scaled_results['silhouette_n_clust_score']) / 2
+
+            # Merge the original results with the scaled results
+            df_merged = pd.merge(results_df, scaled_results, on=results_df.index, how='inner').drop(columns=['key_0'])
+
+            # Sort by the total score in descending order
+            df_merged_sorted = df_merged.sort_values(by='total_score', ascending=False)
+
+            return df_merged_sorted
+    
+    def hierarchical_dendogram_study(self,pca_n_components_range): 
+        # Apply PCA
+        for n_components in pca_n_components_range:
+            # Apply PCA to reduce dimensionality
+            pca = PCA(n_components=n_components,random_state=42)  # Adjust the number of components as needed
+            pca_result = pca.fit_transform(self)
+            # print(f"N_components: {n_components}")
+
+            # Generate the linkage matrix
+            
+        #     linkage_matrix = linkage(pca_result, method='ward')
+
+            # Plot the dendrogram
+            plt.figure(figsize=(12, 8))
+            dendrogram_plot = dendrogram(linkage(pca_result, method='ward'))
+            plt.title(f'Dendrogram for Hierarchical Clustering, n_components: {n_components}')
+            plt.xlabel('Samples')
+            plt.ylabel('Distance')
+            plt.show()
+
+    def hcl_study(self,pca_n_components_range, n_clusters_range):
+
+        # Initialize a list to store the results
+        results = []
+
+        for n_components in pca_n_components_range:
+            # Apply PCA to reduce dimensionality
+            pca = PCA(n_components=n_components, random_state=42)  # Adjust the number of components as needed
+            pca_result = pca.fit_transform(self)
+
+            for n_clusters in n_clusters_range:
+                # Perform Agglomerative Clustering
+                agg_clust = AgglomerativeClustering(n_clusters=n_clusters)
+                agg_labels = agg_clust.fit_predict(pca_result)  # Use pca_result instead of X_pca
+
+                # Calculate the silhouette score
+                silhouette_avg = silhouette_score(pca_result, agg_labels)
+
+                # Append results to the list
+                results.append({
+                    'n_components': n_components,
+                    'n_clusters': n_clusters,
+                    'silhouette_score': silhouette_avg
+                })
+
+        # Convert results to a DataFrame
+        return pd.DataFrame(results)
+
+    def knee_study(self, max_n_component):
+        
+        pca = PCA(n_components=max_n_component, random_state=42)
+        X_pca = pca.fit_transform(self)
+                                
+        neighbors = NearestNeighbors(n_neighbors=2)             # Using NearestNeighbors to find the optimal epsilon value
+        neighbors_fit = neighbors.fit(X_pca)
+        distances, indexes = neighbors_fit.kneighbors(X_pca)
+        
+        distances = np.sort(distances, axis=0)
+        distances = distances[:,1]
+        fig = plt.figure(figsize=(10,10))
+        plt.plot(distances)
+        plt.title(f"N_components = {max_n_component}")
+        plt.show()
+    
+    def dbscan_study(self,eps_values,min_samples,pca_n_components_range=None):
+        # Apply PCA
+        results_list = []  # Initialize the list once, outside the loops
+
+        
+        if pca_n_components_range:
+            for n_component in pca_n_components_range:
+                pca = PCA(n_components=n_component, random_state=42)
+                X_pca = pca.fit_transform(self)
+                
+                # Generate all combinations of parameters
+                dbscan_params = list(product(eps_values, min_samples))
+
+                # Loop through each combination of eps and min_samples
+                for eps, min_s in dbscan_params:
+                    # Fit DBSCAN
+                    db = DBSCAN(eps=eps, min_samples=min_s)
+                    labels = db.fit_predict(X_pca)
+
+                    # Number of clusters in labels, ignoring noise if present
+                    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+                    n_noise_ = list(labels).count(-1)
+                    not_noise_ = len(labels) - n_noise_
+
+                    # Calculate silhouette score, ignoring noise points
+                    if n_clusters_ > 1:  # Silhouette score is not defined for a single cluster
+                        sil_score = silhouette_score(X_pca[labels != -1], labels[labels != -1])
+                    else:
+                        sil_score = None  # Use None for clarity
+
+                    # Append the results to the list (inside the inner loop)
+                    results_list.append({
+                        'eps': eps,
+                        'min_samples': min_s,
+                        'n_clusters': n_clusters_,
+                        'n_noise': n_noise_,
+                        'not_noise': not_noise_,
+                        'silhouette_score': sil_score,
+                        'n_components': n_component
+                    })
+        else:
+            # Generate all combinations of parameters
+            dbscan_params = list(product(eps_values, min_samples))
+            # Loop through each combination of eps and min_samples
+            for eps, min_s in dbscan_params:
+                # Fit DBSCAN
+                db = DBSCAN(eps=eps, min_samples=min_s)
+                labels = db.fit_predict(self)
+
+                # Number of clusters in labels, ignoring noise if present
+                n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+                n_noise_ = list(labels).count(-1)
+                not_noise_ = len(labels) - n_noise_
+
+                # Calculate silhouette score, ignoring noise points
+                # if n_clusters_ > 1:  # Silhouette score is not defined for a single cluster
+                #     sil_score = silhouette_score(self[labels != -1], labels[labels != -1])
+                # else:
+                #     sil_score = None  # Use None for clarity
+
+                valid_mask = labels != -1
+                if valid_mask.sum() > 0:  # Asegurarse de que no todos sean ruido
+                    sil_score = silhouette_score(self[valid_mask], labels[valid_mask])
+                else:
+                    sil_score = None
+
+                # Append the results to the list (inside the inner loop)
+                results_list.append({
+                    'eps': eps,
+                    'min_samples': min_s,
+                    'n_clusters': n_clusters_,
+                    'n_noise': n_noise_,
+                    'not_noise': not_noise_,
+                    'silhouette_score': sil_score,
+                })
+
+        # Convert the results list to a DataFrame
+        return pd.DataFrame(results_list)
